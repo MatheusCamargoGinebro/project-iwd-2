e180bcbacb500913d01cc9c699e74ccead3cadc7
MatheusCamargoGinebro <matheuscginrbro@gmail.com>
2023-05-07 14:11:45 -0300

    navbar-menu add

--- scripts/defaultScripts.js
+++ scripts/defaultScripts.js
@@ -6,7 +6,19 @@ O===========================O
 O===========================O
 
 Este script tem as funções básicas (que todas as páginas do site precisam).
-Ex: ainda não tem :(
+Ex: funcionamento do side-navbar.
 */
 
 console.log('defaultScripts loaded');
+
+const div = document.getElementById('side-navbar-div');
+const checkbox = document.getElementById('nav-bar-activator').addEventListener('change', function() {
+  if (this.checked) {
+    this.style.marginLeft = 60 + "vw";
+    div.style.left = 0;
+  } else {
+    this.style.marginLeft = 0 + "px";
+    div.style.left = -76 + "vw";
+  }
+});
+  
